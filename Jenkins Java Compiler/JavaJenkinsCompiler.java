24c0d54ced41813db621b624881331db86d88e68
Hamit Mizrak <hamitmizrak@gmail.com>
2024-09-26 15:19:14 +0300

    Jenkins Java Compiler

---
+++ Jenkins Java Compiler/JavaJenkinsCompiler.java
@@ -0,0 +1,7 @@
+
+// Class
+public class JavaJenkinsCompiler {
+    public static void main(String [] args){
+        System.out.println("Jenkins Java Maven");
+    } //end psvm
+} //end class
---
+++ Jenkins Java Compiler/Readme.md
@@ -0,0 +1,10 @@
+# Jenkins Java Maven
+[GitHub](hamitmizrak/jenkins-java-maven)
+---
+
+
+## Jenkins
+```sh
+javac compiler
+
+```
